e45d90f4ece7d3556afcc6522c7afd695133c59c
d3mmalition <ecarr.gardner@gmail.com>
2023-01-25 18:56:48 -0500

    added errors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const errorHandler = require("./errors/errorHandler")
+const moviesRouter = require("./movies/movies.router")
+const theaterRouter = require("./theaters/theaters.router")
+const reviewRouter = require("./reviews/reviews.router")
+const notFound = require("./errors/notFound")
+
+// cors for entire app
+app.use(cors());
+app.use(express.json());
+
+//three routers
+app.use("/movies", moviesRouter);
+app.use("/theaters", theaterRouter);
+app.use("/reviews", reviewRouter);
+
+//error handling
+app.use(notFound);
+app.use(errorHandler);
+
 
 module.exports = app;
