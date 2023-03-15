fda51a742f4eed031b40ce367eaa42a0912ae1ea
d3mmalition <ecarr.gardner@gmail.com>
2023-03-15 11:16:01 -0400

    update env

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgres://atjhdrhe:IRnMXWW8oI2HqWLBYJAHzAFw3eXjMakz@batyr.db.elephantsql.com/atjhdrhe",
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
 
 module.exports = {
